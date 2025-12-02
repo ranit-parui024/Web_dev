6a520fde18df7619fbf40e79ab766fa923f460b0
Ranit Parui <ranitparui2005@gmail.com>
2025-12-02 14:35:11 +0530

    update

--- mern-stack/JS/index.js
+++ mern-stack/JS/index.js
@@ -1,1 +1,22 @@
-console.log("STRIKE is coming");
+// let name = "Ranit";
+// let age = 24;
+// // age = 40;
+// console.log(name , age);
+
+// datatypes -> primitive
+// numbers
+
+// let a = 67;
+// let b = 6.7;
+// console.log(a+b);
+
+// stirngs
+// let c = "Stike is coming";
+// let b = 'Anjali';
+
+// console.log(c, b);
+ 
+// bigint -> large integer number
+let num = 5128974354168453n;
+
+console.log(num);
